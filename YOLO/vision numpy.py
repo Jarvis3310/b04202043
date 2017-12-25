c7a5dd95ff0cd31a074eca0bb000933111fdc998
Eason Lin <31880054+B04202043@users.noreply.github.com>
2017-12-25 10:53:03 +0800

    Update vision numpy.py

--- YOLO/vision numpy.py
+++ YOLO/vision numpy.py
@@ -3,6 +3,7 @@
 Created on Sat Nov  4 22:55:12 2017
 
 @author: 林亦盛
+Changing video into numpy data on Windows, since it is unavailable to open videos by opencv on Ubuntu virtual machine. 
 """
 
 import numpy as np
