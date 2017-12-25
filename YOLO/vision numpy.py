419ed0589a8d39717933f3c717f43bd583d8b885
Eason Lin <31880054+B04202043@users.noreply.github.com>
2017-12-25 10:48:41 +0800

    Update vision numpy.py

--- YOLO/vision numpy.py
+++ YOLO/vision numpy.py
@@ -7,34 +7,12 @@ Created on Sat Nov  4 22:55:12 2017
 
 import numpy as np
 import cv2
-'''
-img = cv2.imread('haha.png',0)
-print(img)
-cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-'''
 cap = cv2.VideoCapture()
-ret = cap.open('Vince Carter.mp4')
+ret = cap.open('West.mp4')
 print(ret)
 
-'''
-while(cap.isOpened()):
-    ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.namedWindow('frame', cv2.WINDOW_AUTOSIZE)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(5) & 0xFF == ord('q'):
-        break
-    
-
-cap.release()
-cv2.destroyAllWindows()
-'''
 frames = list()
 i = 0
 while ret:
@@ -42,4 +20,4 @@ while ret:
     if ret:
         frames.append(img)
         i += 1
-np.save('vince_video', frames)
+np.save('west_video', frames)
